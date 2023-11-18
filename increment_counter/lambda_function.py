1b9df5b04768379893dbc09bd98680a9227237de
greqq <lmasanovic@outlook.com>
2023-11-18 17:09:15 +0100

    Minor changes

--- increment_counter/lambda_function.py
+++ increment_counter/lambda_function.py
@@ -6,7 +6,7 @@ from datetime import datetime, timedelta
 # Initialize DynamoDB resource
 dynamodb = boto3.resource('dynamodb')
 
-# Tables
+# Table
 visitor_counter_table = dynamodb.Table('VisitorCounter')
 unique_visitor_table = dynamodb.Table('UniqueVisitors')
 
@@ -22,16 +22,13 @@ def lambda_handler(event, context):
     # Define the timeframe in which a visitor is counted as unique
     timeframe_limit = datetime.now() - timedelta(days=1)
     
-    unique_visits = None
-    
-    # Try to retrieve the unique visitor from the table
     try:
         response = unique_visitor_table.get_item(Key={'hashed_ip': hashed_ip})
         item = response.get('Item')
         
         # If the visitor is new or last visited earlier than the timeframe limit
         if not item or datetime.fromisoformat(item['last_visited']) < timeframe_limit:
-            # Visitor is unique within the timeframe
+            # Update the 'last_visited' timestamp for the visitor
             unique_visitor_table.put_item(
                 Item={
                     'hashed_ip': hashed_ip,
@@ -39,13 +36,12 @@ def lambda_handler(event, context):
                 }
             )
             # Increment the unique visitor count
-            unique_response = visitor_counter_table.update_item(
-                Key={'id': 'unique'},
+            visitor_counter_table.update_item(
+                Key={'id': 'counter'},
                 UpdateExpression='ADD visits :inc',
                 ExpressionAttributeValues={':inc': 1},
                 ReturnValues="UPDATED_NEW"
             )
-            unique_visits = unique_response['Attributes']['visits']
 
     except Exception as e:
         print(e)
@@ -54,22 +50,11 @@ def lambda_handler(event, context):
             'body': json.dumps({'error': "Internal Server Error"})
         }
     
-    # Increment the total visitor count
-    total_response = visitor_counter_table.update_item(
-        Key={'id': 'total'},
-        UpdateExpression='ADD visits :inc',
-        ExpressionAttributeValues={':inc': 1},
-        ReturnValues="UPDATED_NEW"
-    )
+    # Get the current unique visitor count
+    count_response = visitor_counter_table.get_item(Key={'id': 'counter'})
+    unique_visits = count_response.get('Item', {}).get('visits', 0)
     
-    # Prepare the response body
-    body = {
-        'total_visits': total_response['Attributes']['visits']
-    }
-    if unique_visits is not None:
-        body['unique_visits'] = unique_visits
-    
-    # Return the updated counts
+    # Return the unique visitor count
     return {
         'statusCode': 200,
         'headers': {
@@ -77,5 +62,5 @@ def lambda_handler(event, context):
             'Access-Control-Allow-Methods': 'GET,POST',
             'Access-Control-Allow-Headers': 'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'
         },
-        'body': json.dumps(body)
+        'body': json.dumps({'unique_visits': unique_visits})
     }
